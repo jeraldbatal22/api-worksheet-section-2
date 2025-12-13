60d4b67077257683822d60acbfb99c941c207a41
Jerald Batal <jebatal@highlysucceed.com>
2025-12-13 17:06:25 +0800

    commit

--- app.ts
+++ app.ts
@@ -155,9 +155,9 @@ function createSession(): T_Session {
 app.use(errorMiddleware);
 
 if (process.env.NODE_ENV !== "test") {
-  app.listen(8080, async () => {
+  app.listen(3000, '0.0.0.0', async () => {
     // await connectToDatabase();
-    console.log(`Server running at http://localhost:${8080}`);
+    console.log(`Server running at http://localhost:${3000}`);
   });
 }
 
